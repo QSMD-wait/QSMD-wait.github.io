4c44d681a997dc5a4b0996ea9676db600c040104
QSMD-wait <zhusunyiyang2233@163.com>
2025-01-15 08:51:57 +0800

    Site updated: 2025-01-15 08:51:55

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731580625395";
+const VERSION = "1736902247986";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
